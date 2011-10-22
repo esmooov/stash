0b5b60aae3372e7eb9456ec565e4168edbc99c3a
Erik Hinton <erikhinton@Eriks-MacBook-Air.local>
2011-10-22 12:08:21 -0400

    Fast good version of lick, preparallel

--- lick4.hs
+++ lick4.hs
@@ -1,22 +1,38 @@
 {-# LANGUAGE OverloadedStrings, BangPatterns #-}
 import Data.Maybe
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.HashMap.Strict as H
 import Text.Regex.Posix
 import Data.List
 import qualified Data.Char as C
 import Control.Applicative
-import Data.Binary as Bin
+import Data.Binary
+import Data.Binary.Get
+import Data.Int
+
+getChunkList :: Int -> Get [B.ByteString]
+getChunkList n = do
+    left <- remaining
+    if let reach = fromIntegral n in left < reach
+        then do x <- getBytes $ fromIntegral left
+                return [x]
+        else do x <- getBytes n
+                rest <- getChunkList n
+                return (x:rest)
 
 isClassChar a = C.isAlphaNum a || elem a " \'-#@%"
 
-cullWord :: B.ByteString -> B.ByteString
-cullWord w = B.map C.toLower $ B.filter isClassChar w
+cullWord :: BL.ByteString -> BL.ByteString
+cullWord w = BL.map C.toLower $ BL.filter isClassChar w
 
-procTextN :: Int -> B.ByteString -> [(B.ByteString,Int)]
-procTextN n t = H.toList $ foldl' ngram' H.empty lines
-                 where !lines = B.lines t
-                       ngram' !tr !line = ngram tr (B.words line) n
+procTextN :: H.HashMap B.ByteString Int -> Int -> B.ByteString ->  H.HashMap B.ByteString Int
+procTextN tree n t = foldl' ngram' tree lines
+                     where !lines = B.lines t
+                           ngram' !tr !line = ngram tr (B.words line) n
+
+procTextN' :: Int -> [B.ByteString] -> [(B.ByteString,Int)]
+procTextN' n chunks = H.toList $ foldl' (\tr c -> H.filter ( > 2) $ procTextN tr n c) H.empty chunks
 
 ngram :: H.HashMap B.ByteString Int -> [B.ByteString] -> Int -> H.HashMap B.ByteString Int
 ngram tr []           _  = tr    
@@ -32,5 +48,5 @@ procText t = H.toList $ foldl' (\hash word -> updateWord [word] hash) H.empty wo
              where words = B.split ' ' $ B.map (\a -> if a == '\n' then ' ' else a) t
 
 main = do
-       test2 <- cullWord <$> B.readFile "canewobble"
-       print $ filter (\(a,b) -> b > 100) $ sortBy (\(a,b) (c,d) -> compare d b) $ procTextN 3 test2
+       test2 <- runGet (getChunkList 1000000) <$> cullWord <$> BL.readFile "canewobble"
+       print $ filter (\(a,b) -> b > 100) $ sortBy (\(a,b) (c,d) -> compare d b) $ procTextN' 3 test2
