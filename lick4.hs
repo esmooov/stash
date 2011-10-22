1f96b8400b5eca519524cf373a85fd63e85be6fc
Erik Hinton <erikhinton@Eriks-MacBook-Air.local>
2011-10-22 00:01:06 -0400

    Pre binary

--- lick4.hs
+++ lick4.hs
@@ -5,31 +5,32 @@ import qualified Data.HashMap.Strict as H
 import Text.Regex.Posix
 import Data.List
 import qualified Data.Char as C
-
+import Control.Applicative
+import Data.Binary as Bin
 
 isClassChar a = C.isAlphaNum a || elem a " \'-#@%"
 
 cullWord :: B.ByteString -> B.ByteString
 cullWord w = B.map C.toLower $ B.filter isClassChar w
 
-procTextN :: Int -> B.ByteString -> [([B.ByteString],Int)]
-procTextN n t = H.toList $ foldl' ngram H.empty lines
-                 where !lines = B.lines $ cullWord t
-                       ngram tr line = snd $! foldl' breakdown (base,tr) (B.split ' ' line)
-                       base = replicate (n-1) ""
+procTextN :: Int -> B.ByteString -> [(B.ByteString,Int)]
+procTextN n t = H.toList $ foldl' ngram' H.empty lines
+                 where !lines = B.lines t
+                       ngram' !tr !line = ngram tr (B.words line) n
 
-breakdown :: ([B.ByteString], H.HashMap [B.ByteString] Int) -> B.ByteString -> ([B.ByteString], H.HashMap [B.ByteString] Int)
-breakdown (!st@(s:ss),tree) !word = (newStack,expandedWord)
-                                    where !newStack = ss++[word]
-                                          !expandedWord = updateWord (st++[word]) tree
+ngram :: H.HashMap B.ByteString Int -> [B.ByteString] -> Int -> H.HashMap B.ByteString Int
+ngram tr []           _  = tr    
+ngram tr words@(w:ws) n  = ngram newtree ws n
+                           where woz = take n words
+                                 !newtree = updateWord woz tr
 
-updateWord :: [B.ByteString] -> H.HashMap [B.ByteString] Int -> H.HashMap [B.ByteString] Int
-updateWord !w = H.insertWith (+) w 1
+updateWord :: [B.ByteString] -> H.HashMap B.ByteString Int -> H.HashMap B.ByteString Int
+updateWord !w = H.insertWith (+) (B.intercalate " " w) 1
 
-procText :: B.ByteString -> [([B.ByteString],Int)]
+procText :: B.ByteString -> [(B.ByteString,Int)]
 procText t = H.toList $ foldl' (\hash word -> updateWord [word] hash) H.empty words
              where words = B.split ' ' $ B.map (\a -> if a == '\n' then ' ' else a) t
 
 main = do
-       test2 <- B.readFile "canewobble"
+       test2 <- cullWord <$> B.readFile "canewobble"
        print $ filter (\(a,b) -> b > 100) $ sortBy (\(a,b) (c,d) -> compare d b) $ procTextN 3 test2
